a18c66463a6062246c8e300538319e7d1b011c79
Leo <leonardo.loza.20isc@tecsanpedro.edu.mx>
2022-04-05 12:08:34 -0500

    U2_E1v2

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace EjecucionClase_LeonardoLoza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCantidad = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tbCantidad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbCantidad
+            // 
+            this.tbCantidad.Location = new System.Drawing.Point(34, 302);
+            this.tbCantidad.Maximum = 15;
+            this.tbCantidad.Name = "tbCantidad";
+            this.tbCantidad.Size = new System.Drawing.Size(190, 56);
+            this.tbCantidad.TabIndex = 0;
+            this.tbCantidad.Scroll += new System.EventHandler(this.tbCantidad_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 78);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "crear boton";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("a Anti Corona", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(230, 302);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(0, 14);
+            this.lblCantidad.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "crear control";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(61, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 65);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbCantidad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbCantidad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbCantidad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+
+
+
+
+namespace EjecucionClase_LeonardoLoza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblCantidad.Text = tbCantidad.Value.ToString();
+
+
+        }
+
+  
+
+
+
+
+        private void tbCantidad_Scroll(object sender, EventArgs e)
+        {
+            lblCantidad.Text = tbCantidad.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //crear las cajas
+
+            byte cantidadDeCajas = Convert.ToByte(tbCantidad.Value);
+            //convierte bit por bit a un byte
+
+            byte cantidadDeCajas2 = (byte)tbCantidad.Value;
+            //agarra el valor y lo maquilla para que sea un byte pero no hay una conversión como tal
+            //sigue funcionando como byte
+
+            for (int i = 1; i <= tbCantidad.Value; i++)
+            {
+                //Crear cajas de texto
+
+                //el txtcontrol es un objeto, y si vuelve a entrar al for vuelve a crear uno,
+                //cada uno es totalmente independiente
+                TextBox txtControl = new TextBox();
+                //los contoles del cuadro de herramienta vienen de una clase
+
+                txtControl.Name = "txt" + " "+ i.ToString();
+                //le cambio el nombre a la caja de texto para que no se llamen igual
+                txtControl.Tag = i;
+                txtControl.Size = new Size(60, 15);
+                txtControl.Location = new Point(ClientSize.Width/2, 30 *( i+1));
+                //pone una caja que baja cada 30 pixeles 
+                txtControl.Text = txtControl.Name + " "+ i.ToString(); //le asigno adentro un nombre a cada objeto creado
+                //concateno valores (es decir, me dice que caja es
+
+                this.Controls.Add(txtControl);
+
+                //Asociarlo a un evento al método txtClic (se activa el método txtClic cuando
+                //de un clic a uno de los controles, es decir,una caja de texto)
+                txtControl.Click += new EventHandler(txtClic);
+                //cuando de click a un control se va a traer 
+            }
+
+        }
+
+
+        //metodo para usar las cajas, 
+        public void txtClic(object sender, EventArgs e)
+        //en el método checa si es un textbox,
+        {
+
+            string speech = string.Format("El nombre del control es:" + ((TextBox)sender).Name.ToString());
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+          
+
+            while (true)
+            {
+                speechSynthesizer.Speak(speech);
+            }
+            //busca con el sender el tipo de elemento para que se traiga el tag
+            //MessageBox.Show("Control:" + ((TextBox)sender).Tag.ToString());
+            //object sender me permite traer la información en específico del control que tiene el foco
+            //es decir, que solo va a buscar los objetos tipo textbox y me va a enviar la propiedad tag,
+            //pero no guarda nada
+            //el sender solo se trae la info del objeto que trae el foco
+            //y que sea de tipo textbox
+
+            //va a buscar objetos de tipo textbox y se va a traer el tag de ellos
+
+
+        }
+    }
+}
